e02770b282eb1845e87c5ef78ba32491048c1bbb
Emil Martens <emil.martens@gmail.com>
2021-02-11 18:12:13 +0100

    assignment 3 task 1 done

--- assignment_03/SatelliteDynamics.m
+++ assignment_03/SatelliteDynamics.m
@@ -1,11 +1,24 @@
-function [ state_dot ] = SatelliteDynamics( t, x, parameters )
+function [statedot] = SatelliteDynamics(t, x, parameters)
+    position = x(1:3);
+    R = reshape(x(4:12), 3, 3);
+    velocity = x(13:15);
+    omega = x(16:18);
+    omegacross = [
+            0, -omega(3), +omega(2);
+            +omega(3), 0, -omega(1);
+            -omega(2), +omega(1), 0
+            ];
+    inertia = parameters;
+    statedot = [
+            velocity;
+            reshape(R * omegacross, 9, 1);
+            GravityAcceleration(position);
+            -inertia \ omegacross * inertia * omega
+            ];
+end
 
-    % Code your equations here...
-
-    % The code must return in the order you selected, e.g.:
-    %    state_dot =  [velocity;
-    %                  orientation_dot;
-    %                  acceleration (ac);
-    %                  angular acceleration (omega dot)];
-
+function [g] = GravityAcceleration(position)
+    M = 5.972e+24;
+    G = 6.674e-11;
+    g = -G * M * position / norm(position, 2)^3;
 end
